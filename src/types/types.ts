265be4d010251809c460e461675e5db67b779441
Takahiro <tkhr_621@docomo.ne.jp>
2021-10-10 04:02:35 +0900

    レストランの情報の型ファイル

---
+++ src/types/types.ts
@@ -0,0 +1,50 @@
+export interface restaurantData {
+    business_status: string;
+    geometry: Geometry;
+    icon: string;
+    icon_background_color: string;
+    icon_mask_base_uri: string;
+    name: string;
+    opening_hours: Openinghours;
+    photos: Photo[];
+    place_id: string;
+    plus_code: Pluscode;
+    price_level: number;
+    rating: number;
+    reference: string;
+    scope: string;
+    types: string[];
+    user_ratings_total: number;
+    vicinity: string;
+  }
+  
+  interface Pluscode {
+    compound_code: string;
+    global_code: string;
+  }
+  
+  interface Photo {
+    height: number;
+    html_attributions: string[];
+    photo_reference: string;
+    width: number;
+  }
+  
+  interface Openinghours {
+    open_now: boolean;
+  }
+  
+  interface Geometry {
+    location: Location;
+    viewport: Viewport;
+  }
+  
+  interface Viewport {
+    northeast: Location;
+    southwest: Location;
+  }
+  
+  interface Location {
+    lat: number;
+    lng: number;
+  }
