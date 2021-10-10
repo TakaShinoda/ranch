9426582d781a33277a54c14443dcbae05f6e7062
Takahiro <tkhr_621@docomo.ne.jp>
2021-10-11 00:35:06 +0900

    検索結果表示コンポーネント

--- src/Search.tsx
+++ src/Search.tsx
@@ -1,29 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useLonAndLat } from './hooks/useLonAndLat'
 import { Card } from './Card'
+import { restaurantData } from './types/types'
 
 export const Search = () => {
   const [lat, lon] = useLonAndLat()
-  console.log(lat)
-  console.log(lon)
+  const [results, setResults] = useState<restaurantData[]>([])
 
-  //   if (lat && lon) {
-  //     fetch(
-  //       `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=35.6715962,139.4632724&radius=1250&types=restaurant&language=ja&key=${process.env.REACT_APP_GOOGLE_MAP_KEY}`
-  //     )
-  //       .then((response) => response.json())
-  //       .then((data) => console.log(data))
-  //       .catch((error) => {
-  //         console.error('Error:', error)
-  //       })
-  //   }
+  useEffect(() => {
+    let unmounted = false
+    if (!unmounted) {
+      getDate()
+    }
+    return () => {
+      unmounted = true
+    }
+  }, [lon])
+
+  const getDate = async () => {
+    try {
+      // console.log(lat)
+      // console.log(lon)
+      const res = await fetch(
+        `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lon}&radius=1000&types=restaurant&language=ja&key=${process.env.REACT_APP_GOOGLE_MAP_KEY}`
+      )
+      const json = await res.json()
+      setResults(json.results)
+    } catch (err) {
+      console.error(err)
+    }
+  }
 
   return (
     <>
-      <h1>検索</h1>
-      <p>{lat}</p>
-      <p>{lon}</p>
-      <Card />
+      <Card results={results} />
     </>
   )
 }
